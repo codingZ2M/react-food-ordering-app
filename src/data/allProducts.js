5118ef0111059880e273024985af07f490f98d5b
Raj <ssraj74@gmail.com>
2023-01-28 10:56:33 +0530

    2nd commit

--- src/data/allProducts.js
+++ src/data/allProducts.js
@@ -163,6 +163,36 @@ const allProducts = [
             category: 'Watches',
             curatedProduct: false
           },
+          {
+            id: 'WAreRdx05',
+            title: 'Analog Brown Silicone Strap Women Wrist Watch',
+            price: '33.99',
+            image: 'https://images.unsplash.com/photo-1488477304112-4944851de03d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+            qty:1,
+            details: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged',
+            category: 'Watches',
+            curatedProduct: false
+          },
+          {
+            id: 'WAreRdx05',
+            title: 'Analog Brown Silicone Strap Women Wrist Watch',
+            price: '33.99',
+            image: 'https://images.unsplash.com/photo-1621303837174-89787a7d4729?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80',
+            qty:1,
+            details: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged',
+            category: 'Watches',
+            curatedProduct: false
+          },
+          {
+            id: 'WAreRdx05',
+            title: 'Analog Brown Silicone Strap Women Wrist Watch',
+            price: '33.99',
+            image: 'https://images.unsplash.com/photo-1599785209758-243af93bd074?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80',
+            qty:1,
+            details: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged',
+            category: 'Watches',
+            curatedProduct: false
+          },
   ];
   
   export default allProducts;
